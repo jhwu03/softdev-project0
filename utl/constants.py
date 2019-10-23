4beb7051be6a7a697804ac384aacccfd3ea352ed
CreativePenguin <panda501wp@gmail.com>
2019-10-23 14:56:19 -0400

    Nothing works

---
+++ utl/constants.py
@@ -0,0 +1,4 @@
+import sqlite3
+
+DB_FILE = 'data/databases.db'
+DB = sqlite3.connect(DB_FILE)
---
+++ utl/readDB.py
@@ -0,0 +1,6 @@
+import sqlite3
+import constants
+
+c = constants.DB.cursor()
+
+def getUserBlogs(user):
