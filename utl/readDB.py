6c85840bb00e731e057b5e1b3648baed198ca2dd
Jackson <jzou00@stuy.edu>
2019-10-25 18:09:27 -0400

    created getAllBlogs method, this is gonna return most recent entries of all blogs

--- utl/readDB.py
+++ utl/readDB.py
@@ -49,8 +49,8 @@ def displayEntries(userid, blogid):
     c.execute(command)
     q = c.fetchall()
     final = []
-    final.append(q[0][0])
     final.append(q[0][1])
+    final.append(q[0][0])
     for entry in q:
         final.append(entry[2])
 
@@ -59,3 +59,30 @@ def displayEntries(userid, blogid):
     db.close()  #close database
 
     return final
+
+def getAllBlogs():
+    """Returns the last entry for every existing blog. This will be primarily used in the featured section of the home page. Return value will be a list formatted as [blog1, blog2...]. blog1 will be [blog1_name, entry]"""
+    DB_FILE="data/databases.db"
+
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+
+    #==========================================================
+    blogs = []
+    command = "SELECT user_id FROM users;"
+    c.execute(command)
+    q = c.fetchall()
+    for num in range(q[len(q)-1][0]+1):
+        entries = displayBlogs(num)
+        for entry in entries:
+            adding = []
+            adding.append(num)
+            adding.append(entry[0])
+            adding.append(entry[1])
+            adding.append(entry[len(entry)-1])
+            blogs.append(adding)
+    #==========================================================
+    db.commit() #save changes
+    db.close()  #close database
+
+    return blogs
