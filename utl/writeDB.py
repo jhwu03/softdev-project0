420db55f0148ac89b6b0246b1934f938cb2f526a
CreativePenguin <panda501wp@gmail.com>
2019-10-25 11:54:58 -0400

    Finish INSERT INTO SQL

--- utl/writeDB.py
+++ utl/writeDB.py
@@ -4,16 +4,19 @@ c = db.cursor()
 
 def insertIntoSQL(tableName:str, columnVals:list) -> str:
     """ This is an easier way to add things into sql table """
-    cmd = 'INSERT INTO ' + table_name + ' VALUES ('
+    tableHeaders = ''
     for i in columnVals[:-1]:
         if isinstance(i, str):
-            cmd += "" #TODO: Fix this
-        cmd += i + ', '
-     command += columnVals[-1]
-     return command + ');'
+            cmd += '"{}", '.format(i)
+        else:
+            cmd += '{}, '.format(i)
+    if isinstance(columnVals[-1], str):
+         cmd += '"{}"'.format(i)
+    else:
+        cmd += '{}'.format(i)
+    return 'INSERT INTO {} VALUES ({});'.format(tablename, cmd)
 
 
 def addBlog(userID:int, blogTitle:str):
     """ This will allow the user to add a blog.
     It adds to the blogs db """
-    cmd = 'INSERT INTO blogs VALUES
