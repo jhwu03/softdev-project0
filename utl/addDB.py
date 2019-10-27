2e1cae6a60c997b468a7a278ace91df1aefec2ac
Jackson <jzou00@stuy.edu>
2019-10-27 16:33:16 -0400

    addEntry() works now

--- utl/addDB.py
+++ utl/addDB.py
@@ -7,20 +7,20 @@ import sqlite3
 import csv
 
 def addEntry(userid, blogid, entrytext):
-    DB_FILE="../data/databases.db"
+    DB_FILE="data/databases.db"
 
     db = sqlite3.connect(DB_FILE)
     c = db.cursor()
 
     #==========================================================
 
-    command = "SELECT entry_num FROM entries WHERE user_id = 0 AND blog_id = 1;".format(userid, blogid)
+    command = "SELECT entry_num FROM entries WHERE user_id = {} AND blog_id = {};".format(userid, blogid)
     c.execute(command)
     q = c.fetchall();
-    print(q)
+    newentry_id = q[len(q)-1][0]+1
+    command = "INSERT INTO entries VALUES({},{},{},\"{}\");".format(userid, blogid, newentry_id, entrytext)
+    c.execute(command)
     #==========================================================
 
     db.commit() #save changes
     db.close()  #close database
-
-addEntry(0,1, "bleh");
