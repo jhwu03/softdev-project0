938b02cfbccac136d95f90c42490866678d45579
Manfred <mtan00@stuy.edu>
2019-10-27 16:00:06 -0400

    test

--- app.py
+++ app.py
@@ -89,14 +89,16 @@ def home():
 
 
 @app.route("/user/<user>")
-def userPage(user, password, temp1, temp2):
+def userPage(user):
     #username = session[usernae] reutrn specific template
     # if user DNE: return error template
-    #if (user == session["username"]):
-
-    return render_template("user.html",
+    if (user == session["username"]):
+        return render_template("user.html",
+            user = user,
+            myBlogs = readDB.displayBlogs(session["userID"]))
+    return render_template("otherUser.html",
         user = user,
-        allBlogs="TEMP")
+        theirBlogs="TEMP")
 
 @app.route("/<user>/<blog>")
 def blogPage(user):
--- temp.py
+++ temp.py
@@ -1,75 +1,14 @@
-# Jackson Zou
-# SoftDev1 PD 9
-# p00
-
-
-from flask import Flask
-from flask import render_template
-from flask import request
-from flask import redirect
-from flask import url_for
-from utl import createDB
-from utl import checkLogin
-from utl import checkUser
-from utl import newuser
-import sqlite3
-app = Flask(__name__)
-
-createDB.createTable() #always create tables when first run, just in case tables don't exist
-
-@app.route("/")
-def firstLogin():
-    return render_template('login.html',
-        errorMessage = "")
-
-
-@app.route("/login", methods=["GET"])
-def login():
-    if (request.args):
-        username = request.args["username"]
-        password = request.args["password"]
-        userID = checkLogin.checkLogin(username, password) #temp for testing
-        if (userID == -1):
-            return render_template('login.html', errorMessage = "Invalid Credentials")
-        return redirect(url_for("home", username))
-    return render_template("login.html", errorMessage="")
-
-
-@app.route("/register", methods=["GET"])
-def register():
-    if (request.args):
-        username = request.args["username"]
-        password1 = request.args["password1"]
-        password2 = request.args["password2"]
-        if (password1 == password2):
-            if (newuser.addUser(username, password1) == True):
-                return redirect(url_for("home", user=username))
-            return render_template('register.html',
-                errorMessage = "Username already taken")
-        return render_template('register.html',
-            errorMessage = "Passwords do not match. Please try again.")
-    return render_template('register.html', errorMessage = "")
-
-    #add user checks if account exists (returns false). If DNE, enters into database, returns true
-
-
-@app.route("/home")
-def home(user):
-    #user = request.args.get('user')
-    #VARIABLES TO PASS
-    command = "SELECT blog.username, entries.entry "
-    entries = "DISPLAY RECENT ENTRIES"
-    return render_template("home.html",
-        username = user,
-        recentEntries = entries)
-
-@app.route("/<user>")
+@app.route("/user/<user>")
 def userPage(user):
     #username = session[usernae] reutrn specific template
     # if user DNE: return error template
-    return render_template("user.html",
+    if (user == session["username"]):
+        return render_template("user.html",
+            user = user,
+            myBlogs = readDB.displayBlogs(session["userID"]))
+    return render_template("otherUser.html",
         user = user,
-        allBlogs="TEMP")
+        theirBlogs="TEMP")
 
 @app.route("/<user>/<blog>")
 def blogPage(user):
