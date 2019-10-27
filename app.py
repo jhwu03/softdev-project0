22584ddf48c9c07c50b1ea0cd32ccc722d666a41
cohyunoh <coh00@stuy.edu>
2019-10-27 13:02:56 -0400

    session is in sessiona and logout is now completely functional

--- app.py
+++ app.py
@@ -8,13 +8,15 @@ from flask import render_template
 from flask import request
 from flask import redirect
 from flask import url_for
+from flask import session
 from utl import createDB
 from utl import checkLogin
 from utl import newuser
 from utl import readDB
 import sqlite3
+import os
 app = Flask(__name__)
-
+app.secret_key = os.urandom(32) #generates a secret key for session to start
 createDB.createTable() #always create tables when first run, just in case tables don't exist
 
 @app.route("/")
@@ -23,27 +25,32 @@ def firstLogin():
         errorMessage = "")
 
 
-@app.route("/login", methods=["GET"])
+@app.route("/login", methods=["POST"])
 def login():
-    if (request.args):
-        username = request.args["username"]
-        password = request.args["password"]
+    session['username'] = request.form["username"]          # assign username key in session to inputted username
+    session['password'] = request.form["password"]          # assign password key in session to inputted password
+    if (session):
+        username = session['username']
+        password = session['password']
         validLogin = checkLogin.checkLogin(username, password) #temp for testing
         if (validLogin == -1):
             return render_template('login.html', errorMessage = "Invalid Credentials")
-        return redirect(url_for("home", user=username))
+        return redirect(url_for("home"))
     return render_template("login.html", errorMessage="")
 
 
-@app.route("/register", methods=["GET"])
+@app.route("/register", methods=["POST"])
 def register():
-    if (request.args):
-        username = request.args["username"]
-        password1 = request.args["password1"]
-        password2 = request.args["password2"]
+    session['username'] = request.form["username"]          # assign username key in session to inputted username
+    session['password'] = request.form["password1"]          # assign password key in session to inputted password1
+    session['password2'] = request.form["password2"]          # assign password key in session to inputted password2
+    if (session):
+        username = session['username']
+        password1 = session['password']
+        password2 = session['password2']
         if (password1 == password2):
             if (newuser.addUser(username, password1) == True):
-                return redirect(url_for("home", user=username))
+                return redirect(url_for("home"))
             return render_template('register.html',
                 errorMessage = "Username already taken")
         return render_template('register.html',
@@ -52,10 +59,17 @@ def register():
 
     #add user checks if account exists (returns false). If DNE, enters into database, returns true
 
+@app.route("/logout", methods=["GET"])
+def logout():                                               # route logs out the user by getting rid of username and password in session
+    session.pop('username')
+    session.pop('password')
+    if 'password2' in session:
+        session.pop('password2')
+    return redirect(url_for("firstLogin"))                # redirect to beginning
 
 @app.route("/home")
 def home():
-    user = request.args.get('userDATA')
+    user = session['username']
     #VARIABLES TO PASS
     entries = readDB.getAllBlogs()
     return render_template("home.html",
