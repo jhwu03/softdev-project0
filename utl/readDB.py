7542cfc91cd2a79e9d65ea4900e4d11ae950b9f9
Jackson <jzou00@stuy.edu>
2019-10-27 18:46:13 -0400

    changed it for manfred

--- utl/readDB.py
+++ utl/readDB.py
@@ -6,7 +6,7 @@
 import sqlite3
 import csv
 
-def displayBlogs(userid):
+def displayBlogs(username):
     """Returns all blogs of a user specified by their userid. The return value of this method is going to a list of blogs. The overall list will be organized by [blog1, blog2, blog3, ...] and each blog will be a list so blog1 = [blog_id, blog_name, entry1, entry2, ...]"""
     DB_FILE="data/databases.db"
 
@@ -14,7 +14,7 @@ def displayBlogs(userid):
     c = db.cursor()
 
     #==========================================================
-
+    userid = getUserID(username)
     command = "SELECT blogs.user_id, blogs.blog_id, blog_name, entry_num, entry_text FROM blogs INNER JOIN entries ON blogs.user_id = entries.user_id AND blogs.blog_id = entries.blog_id WHERE blogs.user_id = {};".format(userid)
     c.execute(command)
     q = c.fetchall()
@@ -36,7 +36,7 @@ def displayBlogs(userid):
 
     return final
 
-def displayEntries(userid, blogid):
+def displayEntries(username, blogid):
     """Returns all entries for the specified user_id, blog_id combination. Return value will be a list formatted as [blog_id, blog_name, entry1, entry2,...]"""
     DB_FILE="data/databases.db"
 
@@ -44,7 +44,7 @@ def displayEntries(userid, blogid):
     c = db.cursor()
 
     #==========================================================
-
+    userid = getUserID(username)
     command = "SELECT blog_name, entry_num, entry_text FROM blogs INNER JOIN entries ON blogs.user_id = entries.user_id AND blogs.blog_id = entries.blog_id WHERE blogs.user_id = {} AND blogs.blog_id = {};".format(userid, blogid)
     c.execute(command)
     q = c.fetchall()
@@ -121,5 +121,3 @@ def searchUp(search):
         elif search in str(blog[3]):
             results.append(blog)
     return results
-
-print(getUserID("jacksonzou"))
