0180d184bc8b6c395cd2e35bccdb452e5eacb684
cohyunoh <coh00@stuy.edu>
2019-10-27 12:27:35 -0400

    displaying recent entries success

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from flask import url_for
 from utl import createDB
 from utl import checkLogin
 from utl import newuser
+from utl import readDB
 import sqlite3
 app = Flask(__name__)
 
@@ -54,10 +55,9 @@ def register():
 
 @app.route("/home")
 def home():
-    user = request.args.get('user')
+    user = request.args.get('userDATA')
     #VARIABLES TO PASS
-    command = "SELECT blog.username, entries.entry "
-    entries = "DISPLAY RECENT ENTRIES"
+    entries = readDB.getAllBlogs()
     return render_template("home.html",
         username = user,
         recentEntries = entries)
